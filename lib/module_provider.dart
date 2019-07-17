cc934581d2e873fe07b2be4a761658d5881ed8fd
Ricardo Crescenti <ricardo.crescenti@gmail.com>
2019-07-17 15:25:59 -0300

    movido ValueBloc e ValuesBloc para useful_classes

--- lib/module_provider.dart
+++ lib/module_provider.dart
@@ -1,8 +1,6 @@
 library module_pattern;
 
-export 'blocs/value_bloc.dart';
 export 'classes/inject.dart';
-export 'blocs/values_bloc.dart';
 export 'widgets/component.dart';
 export 'widgets/module.dart';
 export 'patterns/controller.dart';
